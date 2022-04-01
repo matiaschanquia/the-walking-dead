9feb65a527f96530f7ad69be7fa7c980b6f181a0
Matias Chanquia <89280882+matiaschanquia@users.noreply.github.com>
2022-04-01 13:05:02 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function abrirNav() {
+    document.getElementById("nav-lateral").style.display = "block";
+}
+
+function cerrarNav() {
+    document.getElementById("nav-lateral").style.display = "none";
+}
